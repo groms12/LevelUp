5c433360ce084f2945c96131c21b2fae89786b6c
groms12 <gms1204@mail.ru>
2021-08-24 21:34:47 +0300

    add all files

---
+++ assets/js/main.js
@@ -0,0 +1,66 @@
+const swiper = new Swiper('.swiper-container', {
+    // Default parameters
+    slidesPerView: 1,
+    spaceBetween: 10,
+    
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+
+  function getTimeRemaining(endtime) {
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
+   
+  function initializeClock(id, endtime) {
+    var clock = document.getElementById(id);
+    var daysSpan = clock.querySelector('.days');
+    var hoursSpan = clock.querySelector('.hours');
+    var minutesSpan = clock.querySelector('.minutes');
+    var secondsSpan = clock.querySelector('.seconds');
+   
+    function updateClock() {
+      var t = getTimeRemaining(endtime);
+   
+      daysSpan.innerHTML = t.days;
+      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+   
+      if (t.total <= 0) {
+        clearInterval(timeinterval);
+      }
+    }
+   
+    updateClock();
+    var timeinterval = setInterval(updateClock, 1000);
+  }
+   
+  var deadline = new Date(Date.parse(new Date()) + 60 * 30 * 1 * 1000); // for endless timer
+  initializeClock('countdown', deadline);
+
+  $(".promo__content__chars__btn").click(function(){
+    window.scrollTo({
+        top: 5000,
+        behavior: "smooth"
+    });
+  })
+
+  $(".ss").keypress(function(event){
+    event = event || window.event;
+    if (event.charCode && event.charCode!=0 && event.charCode!=46 && (event.charCode < 48 || event.charCode > 57) )
+      return false;
+  });
